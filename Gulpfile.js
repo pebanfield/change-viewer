810b8beed4f62c14a5b795171516aead85d43422
P.E. Banfield <pebanfield@gmail.com>
2016-12-01 14:26:34 -0800

    fiddling with nodegit dependency

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,25 +1,25 @@
-var NwBuilder = require('node-webkit-builder');
+var NwBuilder = require('nw-builder');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
 gulp.task('nw', function () {
 
     var nw = new NwBuilder({
-        version: '0.12.2',
+        version: '0.15.4',
         files: './nwapp/**/*',
         macIcns: './icons/icon.icns',
         macPlist: {mac_bundle_id: 'myPkg'},
-        platforms: ['win32', 'win64', 'osx32', 'osx64']
+        platforms: ['win32', 'win64', 'osx64']
     });
 
     // Log stuff you want
     nw.on('log', function (msg) {
-        gutil.log('node-webkit-builder', msg);
+        gutil.log('nw-builder', msg);
     });
 
     // Build returns a promise, return it so the task isn't called in parallel
     return nw.build().catch(function (err) {
-        gutil.log('node-webkit-builder', err);
+        gutil.log('nw-builder', err);
     });
 });
 
--- nwapp/main.js
+++ nwapp/main.js
@@ -4,6 +4,19 @@
 var gui = require('nw.gui');
 var gitParser = require('change-view-service');
 
-gitParser.getHistory();
+console.log("gitParser.getHistory = " + gitParser.getHistory);
+var testPath =
+  '/Users/pebanfield/PROJECTS/PERSONAL/change-view/change-view-service/test/data';
+gitParser.getHistory(testPath).then(function(commits){
+
+  var commitOne = commits[0];
+
+  console.log("one = " + commitOne.revision);
+
+}).catch(function(e) {
+  //Generic catch-the rest, error wasn't TypeError nor
+  //ReferenceError
+  console.log("error : " + e);
+});
 
 gui.Window.get().show();
